76f253417dc0174d1ddb53e81c5a80eff4e9ff6c
Yura <yura-ryabov@mail.ru>
2018-04-15 09:48:19 +0300

    FlyIvan

---
+++ FlyIvan/assets/js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	$('.button').click(function(){
+		$('.button').removeClass('button_hover');
+		$('.button').addClass('button_acrive');
+		$('.img_Ivan').addClass('img_Ivan_anim');
+	});
+});
