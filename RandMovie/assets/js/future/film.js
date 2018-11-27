107093db50efcd5780668796964ded8ae58bfa0e
Yura <yura-ryabov@mail.ru>
2018-11-27 15:28:08 +0700

    Add files via upload

---
+++ RandMovie/assets/js/future/film.js
@@ -0,0 +1,16 @@
+var film = [
+
+	{ 
+		"film_name": "Бойцовкий клуб",
+		"scr": "C:/Users/Positive_go/Desktop/pr/assets/img/film/1.jpg",
+	},
+
+	{ 
+		"film_name": "asd",
+		"scr": "C:/Users/Positive_go/Desktop/pr/assets/img/film/2.jpg",
+	},
+
+	
+];
+
+console.log(film);
